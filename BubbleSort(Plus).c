6ec69d8cebc68cf7b76df017d6610928228b25cb
鲲之大，一锅炖不下 <1274077326@qq.com>
2025-07-31 23:05:52 +0800

    Update BubbleSort(Plus).c

--- BubbleSort(Plus).c
+++ BubbleSort(Plus).c
@@ -7,35 +7,39 @@ void Swap(int *x,int *y)
     *y = *x - *y;
     *x = *x - *y;
 }
-
-bool Max(int x,int y)
+//升序
+bool Greater(int x,int y)
 {
     return x > y;
 }
-
-bool Min(int x,int y)
+//降序
+bool Less(int x,int y)
 {
     return x < y;
 }
 
-//冒泡排序
+//冒泡排序,利用回调函数
 void BubbleSort(int *p,int n,bool(*q)(int,int))
 {
-    int temp,swapped;
     for(int i = 0;i < n-1;++i)
     {
-        temp = 0;
+        bool swapped = false;
         for(int j = 0;j < n-i-1;++j)
         {
             if(q(p[j],p[j+1]))
             {
                 Swap(&p[j],&p[j+1]);
+                swapped = true;//发生交换
             }
+        }
+        if(!swapped)//未交换
+        {
+            break;
         }
     }
 }
 //打印数组
-void PrintSort(int arr[],int n)
+void PrintArray(int arr[],int n)
 {
     for(int i = 0;i < n;++i)
     {
@@ -48,12 +52,12 @@ int main()
     int arr[5]={3,5,8,2,6};
     int n = sizeof(arr)/sizeof(arr[0]);
     printf("升序排列：");
-    BubbleSort(arr,n,Max);
-    PrintSort(arr,n);
+    BubbleSort(arr,n,Greater);
+    PrintArray(arr,n);
     putchar(10);
     printf("降序排列：");
-    BubbleSort(arr,n,Min);
-    PrintSort(arr,n);
+    BubbleSort(arr,n,Less);
+    PrintArray(arr,n);
     putchar(10);
     return 0;
 }
